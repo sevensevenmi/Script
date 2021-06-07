20c5bc09b5c63bb3db56a9af5aeeaef9dfb486cc
dompling <374779789@qq.com>
2021-06-07 10:09:23 +0800

    删除 ck 脚本

--- jd/jd_ck_del.js
+++ jd/jd_ck_del.js
@@ -13,7 +13,8 @@ try {
   if (condition) {
     condition = condition.split(',');
     const newData = cookies.filter(
-      (_, index) => condition.indexOf(`${index + 1}`) === -1);
+      (item, index) => condition.indexOf(`${index + 1}`) === -1 ||
+        condition.indexOf(item.userName) === -1);
     if (cookies.length !== newData.length) {
       $.write(JSON.stringify(newData, null, `\t`), CacheKey);
       $.notify('删除相关 ck 成功', '', `已删除 CK ${cookies.length - newData.length} 条`);
