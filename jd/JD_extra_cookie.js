943a2ba02e1702efe86771e4ab98f319d8006830
dompling <374779789@qq.com>
2021-09-02 10:55:13 +0800

    ck  拼接结束符

--- jd/JD_extra_cookie.js
+++ jd/JD_extra_cookie.js
@@ -86,7 +86,7 @@ function updateJDHelp(username) {
 }
 
 async function GetCookie() {
-  const CV = $request.headers['Cookie'] || $request.headers['cookie'];
+  const CV = `${$request.headers['Cookie'] || $request.headers['cookie']};`;
   console.log(CV);
   if (
     $request.url.indexOf('GetJDUserInfoUnion') > -1 ||
